fba85b9bb48571d33e9ff5609e92532777ee9cd9
sneha shetake <snehashetake.123@gmail.com>
2022-06-04 12:25:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,10 +72,3 @@ setInterval(() => {
 function updateScore(score) {
     scoreCont.innerHTML = "Your Score: " + score
 }
-const reloadtButton = document.querySelector("#reload");
-// Reload everything:
-function reload() {
-    reload = location.reload();
-}
-// Event listeners for reload
-reloadButton.addEventListener("click", reload, false);
